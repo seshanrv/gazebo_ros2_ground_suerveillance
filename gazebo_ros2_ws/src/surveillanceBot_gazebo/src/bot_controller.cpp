6992f6ccb1857f1d9638aaf9b0e6f70df437f2c2
Seshan <seshan.venkita@flandersmake.be>
2021-07-23 10:48:17 +0200

    output type laserscan

--- gazebo_ros2_ws/src/surveillanceBot_gazebo/src/bot_controller.cpp
+++ gazebo_ros2_ws/src/surveillanceBot_gazebo/src/bot_controller.cpp
@@ -1,13 +1,20 @@
 #include <cstdio>
 #include <iostream>
+#include <algorithm>
 #include "rclcpp/rclcpp.hpp"
 #include "rclcpp/publisher_base.hpp"
 #include "geometry_msgs/msg/twist.hpp"
 #include "sensor_msgs/msg/camera_info.hpp"
 #include "sensor_msgs/msg/image.hpp"
+#include "sensor_msgs/msg/laser_scan.hpp"
 
-void subscriber_callback(sensor_msgs::msg::Image::SharedPtr msg){
-  std::cout << "Received image: height = " << msg->height << "width" << msg->width << std::endl;
+void camSubscriber(sensor_msgs::msg::Image::SharedPtr msg){
+  //std::cout << "Received image: height = " << msg->height << "width" << msg->width << std::endl;
+  //std::this_thread::sleep_for(std::chrono::milliseconds(10));
+}
+void laserSubscriber(sensor_msgs::msg::LaserScan::SharedPtr msg){
+  std::cout << "Received laser: size = " << msg->ranges.size() << std::endl;
+  std::this_thread::sleep_for(std::chrono::milliseconds(10));
 }
 
 int main(int argc, char ** argv)
@@ -22,8 +29,8 @@ int main(int argc, char ** argv)
   auto node = rclcpp::Node::make_shared("bot_cntrl");
 
   auto veclocity_pub = node->create_publisher<geometry_msgs::msg::Twist>("/surv_bot/cmd_vel", rclcpp::QoS(10));
-  auto img_sub = node->create_subscription<sensor_msgs::msg::Image>("/camera1/image_raw", rclcpp::QoS(10), &subscriber_callback);
-
+  auto img_sub = node->create_subscription<sensor_msgs::msg::Image>("/camera1/image_raw", rclcpp::QoS(10), &camSubscriber);
+  auto laser_sub = node->create_subscription<sensor_msgs::msg::LaserScan>("/laser1/scan", rclcpp::QoS(10), &laserSubscriber);
   auto  velocity_cmd = geometry_msgs::msg::Twist();
 
   velocity_cmd.linear.x = 1.0;
