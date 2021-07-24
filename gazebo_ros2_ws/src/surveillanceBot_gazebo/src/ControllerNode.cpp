1b2326fc56ff3e70f01d3def017f58e56e5b5140
Seshan <seshan.venkita@flandersmake.be>
2021-07-24 18:29:41 +0200

    trying to stop for obstacle

--- gazebo_ros2_ws/src/surveillanceBot_gazebo/src/ControllerNode.cpp
+++ gazebo_ros2_ws/src/surveillanceBot_gazebo/src/ControllerNode.cpp
@@ -7,6 +7,7 @@ using namespace std::chrono_literals;
 ControllerNode::ControllerNode() : Node("controller_node")
 {
     twist_publisher_ = this->create_publisher<Twist>("/surv_bot/cmd_vel", rclcpp::QoS(10));
+    // RCLCPP_INFO(this->get_logger, "Publishing to /surv_bot/cmd_vel");
     laser_subscriber_ = this->create_subscription<LaserScan>("/laser1/scan", rclcpp::QoS(10), std::bind(&ControllerNode::laser_sub_callback, this, std::placeholders::_1));
     image_subscriber_ = this->create_subscription<Image>("/camera1/image_raw", rclcpp::QoS(10), std::bind(&ControllerNode::cam_sub_callback, this, std::placeholders::_1));
 
@@ -18,6 +19,7 @@ void ControllerNode::publisher_callback()
 {
     compute_twist_cmd();
     twist_publisher_->publish(ControllerNode::twist_cmd_);
+
 }
 
 void ControllerNode::laser_sub_callback(LaserScan::SharedPtr msg)
@@ -35,8 +37,12 @@ void ControllerNode::cam_sub_callback(Image::SharedPtr msg)
 }
 
 void ControllerNode::compute_twist_cmd()
-{
-    twist_cmd_.linear.x = 1.0;
+{   
+    float range_max = laser_scan_->range_max;
+    if (std::any_of(laser_scan_->ranges.begin(), laser_scan_->ranges.end(), [range_max](float range){return range < range_max;}))
+        twist_cmd_.linear.x = 0.0;
+    else
+        twist_cmd_.linear.x =1.0;
 }
 int main(int argc, char* argv[])
 {
